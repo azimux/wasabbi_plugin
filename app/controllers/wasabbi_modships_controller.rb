ba5b7e9fd338587460e4fcc618b54605b32359df
Miles Georgi <azimux@gmail.com>
2009-03-22 21:53:06 -0700

    changed modships to only be manageable by admins

--- app/controllers/wasabbi_modships_controller.rb
+++ app/controllers/wasabbi_modships_controller.rb
@@ -1,23 +1,36 @@
 class WasabbiModshipsController < ApplicationController
+  before_filter do |controller|
+    controller.instance_eval do
+      if params[:id]
+        params[:forum_id] ||= WasabbiModship.find(params[:id]).forum_id
+      elsif params[:wasabbi_modship]
+        params[:forum_id] ||= params[:wasabbi_modship][:forum_id]
+      end
+    end
+  end
+
+  wasabbi_require_login
+  wasabbi_require_admin
+
   # GET /wasabbi_modships
   # GET /wasabbi_modships.xml
   def index
     WasabbiModship.transaction do
       @wasabbi_modships = WasabbiModship.find(:all)
-    
+
       respond_to do |format|
         format.html # index.html.erb
         format.xml  { render :xml => @wasabbi_modships }
       end
     end
   end
-  
+
   # GET /wasabbi_modships/1
   # GET /wasabbi_modships/1.xml
   def show
     WasabbiModship.transaction do
       @wasabbi_modship = WasabbiModship.find(params[:id])
-       
+
       respond_to do |format|
         format.html # show.html.erb
         format.xml  { render :xml => @wasabbi_modship }
@@ -37,7 +50,7 @@ class WasabbiModshipsController < ApplicationController
       end
     end
   end
-  
+
   # GET /wasabbi_modships/1/edit
   def edit
     WasabbiModship.transaction do
@@ -89,7 +102,7 @@ class WasabbiModshipsController < ApplicationController
     WasabbiModship.transaction do
       @wasabbi_modship = WasabbiModship.find(params[:id])
       @wasabbi_modship.destroy
-    
+
       respond_to do |format|
         format.html { redirect_to(wasabbi_modships_url) }
         format.xml  { head :ok }
