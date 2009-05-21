c8687871aab54ae8640003c8300ace76bbd95315
Miles Georgi <azimux@gmail.com>
2009-05-21 02:12:21 -0700

    make resource_url find images in an extension agnostic manner

--- lib/wasabbi_theme.rb
+++ lib/wasabbi_theme.rb
@@ -12,12 +12,56 @@ class WasabbiTheme
     File.join Wasabbi.wasabbi_root, 'themes', name
   end
 
-  def theme_url
-    "/vendor/plugins/wasabbi_plugin/themes/#{name}/"
+  def img_dir
+    File.join Wasabbi.wasabbi_root, 'themes', name, 'images'
   end
 
+  def theme_url
+    "/vendor/plugins/wasabbi_plugin/themes/#{name}"
+  end
+
+  private
+  def img_lookup img
+    img = img.gsub(/\.img$/, "")
+    retval = img_cache[img]
+
+    unless retval
+      clear_img_cache
+      retval = img_cache[img]
+    end
+
+    retval
+  end
+
+  def img_cache
+    if @img_cache
+      @img_cache
+    else
+      @img_cache = {}
+
+      Dir.entries(img_dir) do |p|
+        bname = File.basename(p, ".*")
+
+        if File.extname(p) =~ /^\.(gif|png|jpg|jpeg|bmp)$/i
+          raise "Duplicate image name" if @img_cache[bname]
+
+          @img_cache[bname] = File.basename(p)
+        end
+      end
+    end
+  end
+
+  def clear_img_cache
+    @img_cache = nil
+  end
+  public
+
   def resource_url resource
-    "#{theme_url}#{resource}"
+    if resource =~ /\.img$/
+      "#{theme_url}/images/#{img_lookup(resource)}"
+    else
+      "#{theme_url}/#{resource}"
+    end
   end
 
   def find_resouce(resource)
